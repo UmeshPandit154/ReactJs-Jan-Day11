7d5c798f6fcdf09163b087199a72cdd3b11f3b0e
UmeshPandit264 <coderprajapati264@gmail.com>
2025-01-29 10:14:22 +0530

    first commit

---
+++ myapp11/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { BrowserRouter as GrandParent,Routes as Parent, Route as Child } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import NotFound from './pages/NotFound'
+
+
+
+export default function App() {
+    return (
+        < GrandParent>
+             <Parent>
+                <Child path="/" element={<Home />}></Child>
+                <Child path="/login" element={<Login />}></Child>
+                <Child path="/register" element={<Register />}></Child>
+                <Child path="*" element={<NotFound />}></Child>
+             </Parent>
+        </GrandParent>
+    )
+}
